9bdbdd129a508f4652789858ecb48dedf763c5a5
hah37210 <haydar.hamed-jaber@st.oth-regensburg.de>
2022-11-04 18:10:14 +0100

    Homeview responsive Teil2

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -10,44 +10,50 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   Widget createButton(BuildContext context, String buttonName,int iconCodePoint,String heroTag, func) {
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: FloatingActionButton.extended(
-        heroTag: heroTag,
-        //extendedPadding: EdgeInsets.fromLTRB(20, 30, 20, 30),
-        //extendedIconLabelSpacing: 2,
-        splashColor: Colors.lightGreen,
-        backgroundColor: Color(0xFF00DFFF),
-        icon: Icon(
-            shadows: const <Shadow>[
-             Shadow(
-               offset: Offset(0.0, 0.0),
-                blurRadius: 5.0,
-                color: Colors.black,
-              ),
-            ],
-            size: 50,
-            color: Colors.white,
-            IconData(iconCodePoint,fontFamily: 'MaterialIcons')),
-        onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => func)),
-        label: Text(
-          buttonName,
-          style: const TextStyle(
-            shadows: <Shadow>[
-              Shadow(
-                offset: Offset(0.0, 0.0),
-                blurRadius: 5.0,
-                color: Colors.black,
-              ),
-            ],
-            color: Colors.white,
-            fontSize: 30,
-            fontFamily: "Modak",
-          ),
-        ),
-      ),
-    );
+    return Scaffold(
+        floatingActionButton:
+            Container(
+              alignment: Alignment.center,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  width: MediaQuery.of(context).size.width *0.80,
+                  child: FloatingActionButton.extended(
+                    heroTag: heroTag,
+                    splashColor: Colors.lightGreen,
+                    backgroundColor: const Color(0xFF00DFFF),
+                    icon: Icon(
+                        shadows: const <Shadow>[
+                          Shadow(
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 5.0,
+                            color: Colors.black,
+                          ),
+                        ],
+                        size: MediaQuery.of(context).size.height * 0.10,
+                        color: Colors.white,
+                        IconData(iconCodePoint,fontFamily: 'MaterialIcons')),
+                    onPressed: () => Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => func)),
+                    label: Text(
+                      buttonName,
+                      style: TextStyle(
+                        shadows: const <Shadow>[
+                          Shadow(
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 5.0,
+                            color: Colors.black,
+                          ),
+                        ],
+                        color: Colors.white,
+                        fontSize: MediaQuery.of(context).size.height * 0.04,
+                        fontFamily: "Modak",
+                      ),
+                    ),
+                  ),
+
+                )
+            )
+        );
   }
 
   @override
@@ -64,38 +70,38 @@ class HomeScreen extends StatelessWidget {
                     Container(
                       alignment: Alignment.center,
                       width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height*0.30,
+                      height: MediaQuery.of(context).size.height*0.3,
                       child: Image.asset("assets/images/Homescreen_BIG.png"),
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*0.10,
                         alignment: Alignment.center,
-                            child: createButton(context, "   Start             ", 0xf2af, "btn1", const StartScreen()),
+                            child: createButton(context, "Start", 0xf2af, "btn1", const StartScreen()),
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*0.10,
                           alignment: Alignment.center,
-                            child: createButton(context, "    Fortschritt ",0xf05ae, "btn2", const HighScoreScreen()),
+                            child: createButton(context, "Fortschritt",0xf05ae, "btn2", const HighScoreScreen()),
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*0.10,
                           alignment: Alignment.center,
-                          child: createButton(context, "    Anleitung   ",0xe309, "btn3", const HighScoreScreen()),
+                          child: createButton(context, "Anleitung",0xe309, "btn3", const HighScoreScreen()),
                     ),
                     Container(
                           width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*0.10,
                           alignment: Alignment.center,
-                          child: createButton(context, "   Einstellung ",0xe57f, "btn4", const HighScoreScreen()),
+                          child: createButton(context, "Einstellung",0xe57f, "btn4", const HighScoreScreen()),
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height*0.10,
                           alignment: Alignment.center,
-                        child: createButton(context, "    Impressum ",0xe33c, "btn5", const HighScoreScreen())
+                        child: createButton(context, "Impressum",0xe33c, "btn5", const HighScoreScreen())
                     ),
                   ],
               );
@@ -117,6 +123,7 @@ class HomeScreen extends StatelessWidget {
                               alignment: Alignment.center,
                               child: createButton(context, "   Start             ", 0xf2af, "btn1", const StartScreen()),
                             ),
+                            const Padding(padding: EdgeInsets.all(10)),
                             Container(
                               width: MediaQuery.of(context).size.width * 0.40,
                               height: MediaQuery.of(context).size.height*0.10,
@@ -131,6 +138,7 @@ class HomeScreen extends StatelessWidget {
                               alignment: Alignment.center,
                               child: createButton(context, "   Einstellung ",0xe57f, "btn4", const HighScoreScreen()),
                             ),
+                            const Padding(padding: EdgeInsets.all(10)),
                             Container(
                                 width: MediaQuery.of(context).size.width * 0.40,
                                 height: MediaQuery.of(context).size.height*0.10,
